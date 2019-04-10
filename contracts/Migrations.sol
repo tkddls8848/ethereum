513e979f4e8bf57c6035d02d5a03d206386771aa
tkddls8848 <tkddls8848@gmail.com>
2019-04-10 18:33:49 +0900

    ADD .gitignore

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,10 +1,10 @@
-pragma solidity ^0.4.23;
+pragma solidity ^0.4.24;
 
 contract Migrations {
   address public owner;
   uint public last_completed_migration;
 
-  constructor() public {
+  constructor public {
     owner = msg.sender;
   }
 

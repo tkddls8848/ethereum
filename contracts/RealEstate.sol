6d4b0d3225f16e88ae61cb2ade52bdb95e42f6cb
tkddls8848 <tkddls8848@gmail.com>
2019-04-11 17:47:16 +0900

    make buyRealEstate(), Transfer Ethereum

--- contracts/RealEstate.sol
+++ contracts/RealEstate.sol
@@ -1,9 +1,25 @@
 pragma solidity ^0.4.23;
 
 contract RealEstate {
+    struct Buyer {
+        address buyerAddress;
+        bytes32 name;
+        uint age;
+    }
+
     address public owner;
+    address[10] public buyers;
+    mapping(uint => Buyer) public buyerInfo;
 
     constructor() public {
         owner = msg.sender;
+    }
+
+    function buyRealEstate(uint _id, bytes32 _name, uint _age) public payable {
+        require(_id >= 0 && _id <= 9);
+        buyers[_id] = msg.sender;
+        buyerInfo[_id] = Buyer(msg.sender, _name, _age);
+
+        owner.transfer(msg.value);
     }
 }
